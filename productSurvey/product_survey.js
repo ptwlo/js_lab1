be23aa8f26fc9827754d0c07a53675018e3612cb
ptwlo <petterlopez003@gmail.com>
2025-03-05 15:51:54 -0500

    added user experience feedback to the form

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -7,6 +7,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById("userExperience").value;
 
     alert('Thank you for your valuable feedback');
 
@@ -17,7 +18,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
-
+    document.getElementById('userExperienceDisplay').innerHTML = userExperience;
     document.getElementById('userInfo').style.display = 'block';
 
 
