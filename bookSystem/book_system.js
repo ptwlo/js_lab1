ef5886c637374daacdc5041bc69c2659b9600c77
ptwlo <petterlopez003@gmail.com>
2025-03-07 18:21:13 -0500

    delete function added

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,8 @@ let books = [];
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -35,3 +36,9 @@ let books = [];
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
  }
+
+ function deleteBook(index){
+    books.splice(index, 1);
+    showbooks();
+ }
+{}
