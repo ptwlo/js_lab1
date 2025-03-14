96c8ddcb5289bbfc83c4459a74366c0361666c10
ptwlo <petterlopez003@gmail.com>
2025-03-14 10:36:34 -0400

    weather app done

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,24 @@
+function showweatherDetails(event){
+
+    event.preventDefault();
+
+    const city = document.getElementById('city').value;
+    const apiKey = '76944ff116eb11f1e3cd99b178e201cb';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                     <p>Temperature: ${data.main.temp} &#8451;</p>
+                                     <p>Weather: ${data.weather[0].description}</p>`
+            })
+            .catch(error => {
+                console.error('Error fetchin weather:', error);
+                const weatherInfo = document.getElementById('weatherInfo');
+                weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again</p>`;
+            })
+}
+
+document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
